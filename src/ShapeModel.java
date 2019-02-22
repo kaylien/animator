f78bfb94a620a4a4ebf268990608ec4f487550b8
Kaylien Phan <kaylienphan@gmail.com>
2019-02-22 15:02:10 -0500

    added getter and setter methods for shape class

--- src/ShapeModel.java
+++ src/ShapeModel.java
@@ -1,2 +1,19 @@
 public interface ShapeModel {
+
+  /**
+   * What does the model represent?
+   *
+   * The model should be able to support various kinds of 2D shapes, although currently we have
+   * described only rectangles and ovals.
+   *
+   * The model should support at least describing the size, position, and color of shapes, though
+   * you might support additional features.
+   *
+   * Remember to think from not only the implementor’s perspective (the person that is implementing
+   * the model) but also the client perspective (the people or classes that are using the model).
+   * What operations might they reasonably want to perform? What observations might they reasonably
+   * want to make?
+   */
+
+
 }
