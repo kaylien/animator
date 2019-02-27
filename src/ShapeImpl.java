6f8d21270c3ccbce6c98669a4972db8f0b14882e
Kaylien Phan <kaylienphan@gmail.com>
2019-02-27 18:26:57 -0500

    used shapeimpl

--- src/ShapeImpl.java
+++ src/ShapeImpl.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.NavigableMap;
 import java.util.Set;
 
 public abstract class ShapeImpl implements ShapeInt{
@@ -11,7 +12,10 @@ public abstract class ShapeImpl implements ShapeInt{
   private Dimension dimension;
 
   // Integer will be tick
-  HashMap<Integer, List<Command>> commands;
+//  HashMap<Integer, List<Command>> commands;
+  HashMap<Integer, Command> commands;
+//  NavigableMap<Integer, Command> commands;
+  //NavigableMap?????? It finds those closeby keys
 
   /**
    *
@@ -39,23 +43,92 @@ public abstract class ShapeImpl implements ShapeInt{
   }
 
   private void addCommand(Command command) {
-    int key = command.getT();
-
-    if (!this.validCommand(command)) {
-      throw new IllegalArgumentException("Invalid Command");
-    }
-
-    if (commands.containsKey(key)) {
-      commands.get(key).add(command);
-    }
-    else {
-      commands.put(command.getT(), new ArrayList<>());
-    }
+//    int key = command.getT();
+//
+//    if (!this.validCommand(command)) {
+//      throw new IllegalArgumentException("Invalid Command");
+//    }
+//
+//    if (commands.containsKey(key)) {
+//      commands.get(key).add(command);
+//    }
+//    else {
+//      commands.put(command.getT(), new ArrayList<>());
+//    }
   }
+
+  /**
+   * Given a command, what is it changing?
+   * Get tick last prior to the start time of the command, pull out the command
+   * Compare that command to the given command
+   * What's changing?
+   * @param command
+   * @return
+   */
 
   private boolean validCommand(Command command) {
+//    Collection<List<Command>> listOfCommands = commands.values();
+//    for (List<Command> list : listOfCommands) {
+//      for (Command c : list) {
+//
+//      }
+//    }
+//
+//    int key = command.getT();
+//    if (commands.get(command.getT()) == null) {
+//      //get the command right before and right after to the new command
+//      int lastKey = commands.lowerKey(command.getT());
+//      int nextKey = commands.higherKey(command.getT());
+//      Command lastCommand = commands.get(lastKey);
+//      Command nextCommand = commands.get(nextKey);
+//
+//      List<Variable>
+//    }
+//    else {
+//      Collection<Command> listOfCommands = commands.values();
+//      //compare the existing command to the new command
+//    }
+//
     return true;
   }
+
+
+  private enum Variable {
+    COLOR, DIMENSION, POSITION;
+  }
+
+  private boolean isChangingSameVar(List<Variable> v1, List<Variable> v2) {
+    for (Variable var : v1) {
+      for (Variable var2: v2) {
+        if (var == var2) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  static private List<Variable> whatVarsChanging(Command c, Command otherC) {
+    List<Variable> list = new ArrayList<>();
+    if (c.getColor() != otherC.getColor()) {
+      list.add(Variable.COLOR);
+    } else if (c.getPosition() != otherC.getPosition()) {
+      list.add(Variable.POSITION);
+    } else if (c.getDimension() != otherC.getDimension()) {
+      list.add(Variable.DIMENSION);
+    }
+    return list;
+  }
+
+//  private boolean isSameTimeRange(Command c1, Command c2) {
+//    int c1T = c1.getT();
+//    int c2T = c2.getT();
+//    int c1eT = c1.getEt();
+//    int c2eT = c2.getEt();
+//
+//    return c1T > c2T && c1eT < c2eT || c1T > c2T && c1T < c2eT || c2T > c1T && c2T < c1eT;
+//  }
+
 /*
   private boolean validCommand(Command command) {
     for (c : this.commands) {
@@ -86,14 +159,15 @@ public abstract class ShapeImpl implements ShapeInt{
   }
 
   public String getCommands() {
-    Collection<List<Command>> values = commands.values();
-    StringBuilder sb = new StringBuilder();
-
-    for (List<Command> value : values) {
-      sb.append(getTickCommands(value));
-    }
-
-    return sb.toString();
+//    Collection<List<Command>> values = commands.values();
+//    StringBuilder sb = new StringBuilder();
+//
+//    for (List<Command> value : values) {
+//      sb.append(getTickCommands(value));
+//    }
+//
+//    return sb.toString();
+    return "";
   }
 
   public String getTickCommands(List<Command> list) {
