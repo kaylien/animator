b6382f3364fa95a5fb0987513eca7971b9d4d510
ericjichung <ericjichung@gmail.com>
2019-03-01 14:22:48 -0500

    Ignoring Main Method
    - .gitignore

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
   public static void main(String[] args) {
     ShapeImpl s= new Rectangle(20,30,40,10,100,200,50);
     Command c= new Command(20,30,40,10,100,200,50,1,1);
+    Command c1= new Command(20,30,40,10,100,200,12,1,1);
+    Command c2= new Command(20,30,40,10,100,200,50,1,1);
     s.addCommands(c);
+    s.addCommands(c1);
   }
 }
--- test/AnimationModelImplTest.java
+++ test/AnimationModelImplTest.java
@@ -1,4 +1,5 @@
 import javafx.animation.Animation;
+import javafx.scene.shape.Shape;
 import org.junit.Test;
 import org.junit.Assert.*;
 
@@ -15,14 +16,15 @@ public class AnimationModelImplTest {
   AnimationModel queryCommandPass = new AnimationModelImpl();
   AnimationModel queryCommandFail = new AnimationModelImpl();
   AnimationModel queryShapePass = new AnimationModelImpl();
-  AnimationModel qeuryShapeFail = new AnimationModelImpl();
+  AnimationModel queryShapeFail = new AnimationModelImpl();
 
   //queryShape
 
-  //addCommands
+  //addCommand
 
+  @Test
   void addCommandsI() {
-
+    assertEquals();
   }
   //addShape
 
